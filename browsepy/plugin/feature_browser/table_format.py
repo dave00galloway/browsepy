0cb5dfe4a3d26bef052df61157c15d18c2dae95e
david galloway <david.galloway@yahoo.co.uk>
2020-03-09 22:23:11 +0000

    1 - fixed table ordering

--- browsepy/plugin/feature_browser/table_format.py
+++ browsepy/plugin/feature_browser/table_format.py
@@ -35,10 +35,8 @@ class TableFormatSummary(Table):
             items.append(TableFormatEntry(suite_summary=summary, **kwargs))
         for summary in suite_summary.features.values():
             items.append(TableFormatEntry(suite_summary=summary, **kwargs))
+        items.sort(key=lambda x: x.urlpath)
         super().__init__(items)
 
-    # def table_format(self):
-    #     pass
-
     def sort_url(self, col_id, reverse=False):
-        pass
+        list(self.items).sort(key=lambda x: x.urlpath, reverse=reverse)
